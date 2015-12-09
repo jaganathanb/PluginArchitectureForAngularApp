93d4c421171ba271c4a888b2867787b4e679e20e
Jaganathan B <jaganathan.eswaran@gmail.com>
2015-12-09 19:21:01 +0530

    corrected the typo

--- app/common/eslint-plugin-myrules/lib/rules/directive-name-prefix.js
+++ app/common/eslint-plugin-myrules/lib/rules/directive-name-prefix.js
@@ -1,26 +1,16 @@
-module.exports = function(context) {
-
+function rule(context) {
     'use strict';
-
-    function report(node, name, prefix){
-
-        context.report(node, 'The {{directive}} directive should be prefixed by {{prefix}}', {
-            directive: name,
-            prefix: prefix
-        });
-    }
-
     return {
-
         'CallExpression': function(node) {
-
             var prefix = context.options[0];
             var callee = node.callee;
             if (callee.type === 'MemberExpression' && callee.property.name === 'directive') {
                 var name = node.arguments[0].value;
-
                if(name !== undefined && !(name.indexOf(prefix) === 0)){
-                    report(node, name, prefix);
+                    context.report(node, 'The {{directive}} directive should be prefixed with {{prefix}}', {
+                        directive: name,
+                        prefix: prefix
+                    });
                 }
 
             }
@@ -28,3 +18,5 @@ module.exports = function(context) {
     };
 
 };
+
+module.exports = rule;
