d9c36049328f259072d78ba77fce1d40c5c4014f
jaganathan.eswaran@gmail.com <jaganathan.eswaran@gmail.com>
2015-12-16 16:27:18 +0530

    Added doc to ignore

--- shell/src/configs.js
+++ shell/src/configs.js
@@ -1,28 +1,17 @@
-/**
- * @mixin core/config
- */
-
 let configs = {
-/**
-*    @summary run
-*    @memberof core/config
-*    @desc this function will set the default configurations once the application is loaded.
-*/
+
   RUN: ['$rootScope', '$location', 'authService', '$translate', () => {
 
   }],
 
-  /**
-    *  @summary routes
-    *   @memberof core/config
-    *  @desc This function will register the routes for the application
-  */
   ROUTES: require('./routes').default,
-  /**
-    *  @summary translation
-    *   @memberof core/config
-    *  @desc this function will set the locale location to load the locale on demand based on the user profile.
-  */
+
+  DEBUG_MODE: ['$compileProvider', 'DEBUG_MODE', ($compileProvider, DEBUG_MODE) => {
+    if (!DEBUG_MODE) {
+      $compileProvider.debugInfoEnabled(false);// disables AngularJS debug info
+    }
+  }],
+
   TRANSLATION: ['localeServiceProvider', 'tmhDynamicLocaleProvider', (localeServiceProvider, tmhDynamicLocaleProvider) => {
     tmhDynamicLocaleProvider.localeLocationPattern('{{locale}}.js');
   }]
